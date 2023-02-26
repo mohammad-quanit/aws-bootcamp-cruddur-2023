1f34511a6cf268c93e252b6a181eda418a8c1518
MQuanit <muhammadquanit@gmail.com>
2023-02-26 17:06:32 +0000

    add: week2 work

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+# BE Services ----
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -14,7 +15,26 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Honeycomb deps ----
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+# Initialize automatic instrumentation with Flask
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
